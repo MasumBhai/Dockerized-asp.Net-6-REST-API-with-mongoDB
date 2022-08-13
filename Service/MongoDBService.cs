488fb863e7fd1d8147ccdf9fcd9f822b74ac551c
MasumBhai <abdullahmasum6035@gmail.com>
2022-08-13 14:37:22 +0600

    everything invoked using service

---
+++ Service/MongoDBService.cs
@@ -0,0 +1,22 @@
+﻿using DotNet_6_REST_API_with_mongoDB.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace DotNet_6_REST_API_with_mongoDB.Service
+{
+    public class MongoDBService
+    {
+        public async Task<List<Playlist>> GetAsync() { }
+        public async Task CreateAsync(Playlist playlist) { }
+        public async Task AddToPlaylistAsync(string id, string movieId) { }
+        public async Task DeleteAsync(string id) { }
+
+        private readonly IMongoCollection<Playlist> _playlistCollection;
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _playlistCollection = database.GetCollection<Playlist>(mongoDBSettings.Value.CollectionName);
+        }
+    }
+}
