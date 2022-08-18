be06b59740105b306bfc4ed8842684a782d2632e
MasumBhai <abdullahmasum6035@gmail.com>
2022-08-18 22:33:30 +0600

    Cross Origin Request Policy updated

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ await Bootstrapper
     .RunAsync();
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigin = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
@@ -18,10 +19,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Enabling Cors
+
+// Building specific origin supported Cors (change origin here before deploying)
 builder.Services.AddCors(c =>
 {
-    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    c.AddPolicy(MyAllowSpecificOrigin, policy => policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
 });
 
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
@@ -32,7 +34,8 @@ var app = builder.Build();
 app.MapHealthChecks("/health");
 
 // Enabling Cors
-app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors(MyAllowSpecificOrigin);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
